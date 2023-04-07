84aaf7531b80fa4829581a67d3ad7c97820d3132
MilenaMed <milenamfm@gmail.com>
2023-04-07 16:00:59 -0300

    POST /sign-up
    Recebe o parametro username e avatar e retorna ok na mensagem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
-app.get("/hello", (request, response) => {
-    response.send('TWEETEROO')
+app.get("/tweets", (request, response) => {
+    const tweets =
+        [
+            {
+                username: "bobesponja",
+                avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+                tweet: "Eu amo hambúrguer de siri!"
+            }
+        ]
+    response.send(tweets)
 });
 
-app.listen(5000);
+app.post("/sign-up", (request, response) => {
+    request.body
+    
+    const novoUsuário =
+        [
+            {
+                username: "bobesponja",
+                avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+            }
+        ]
+    response.send("OK")
+})
+
+app.listen(5000); 
