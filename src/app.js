f09d63e81bc0e43adf43accc780849647c36bc2e
MilenaMed <milenamfm@gmail.com>
2023-04-07 17:50:18 -0300

    Post /sign-up
    - Salvar esse usuário num array de usuários do servidor.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
+//Ferramentas
 app.use(cors())
 app.use(express.json())
+
+//Globais
+const usuarios = []
 
 app.get("/tweets", (request, response) => {
     const tweets =
@@ -17,16 +22,30 @@ app.get("/tweets", (request, response) => {
     response.send(tweets)
 });
 
+// Criar usuário
 app.post("/sign-up", (request, response) => {
-    request.body
-    
     const novoUsuário =
         [
             {
-                username: "bobesponja",
-                avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+                username: request.body.username,
+                avatar: request.body.avatar
+             }
+        ]
+    usuarios.push(novoUsuário) 
+    console.log(usuarios)   
+    response.send("OK")
+})
+
+app.post("/tweets", (request, response) => {
+    const tweet =
+        [
+            {
+                username: request.body.username,
+                tweet: request.body.tweet
             }
         ]
+
+    tweets.push(tweet)
     response.send("OK")
 })
 
