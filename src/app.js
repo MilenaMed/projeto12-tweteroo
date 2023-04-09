40d0410c32912661fb25a979d0916f8e5307b7e1
MilenaMed <milenamfm@gmail.com>
2023-04-09 18:25:12 -0300

    POST TWEETS
    Correção do post pra pegar as imagens e salvar tweets

--- src/app.js
+++ src/app.js
@@ -8,46 +8,52 @@ app.use(cors())
 app.use(express.json())
 
 //Globais
-const usuarios = [];
-const tweets =[
-
-];
-
+let usuarios = [];
+const tweets = [];
 
 //Tweets já criados
 app.get("/tweets", (request, response) => {
-        if(tweets.length > 10){
-            const ultimosTweets = tweets.slice(-10)
-            response.send(ultimosTweets)
-        }
-        else{
-            response.send(tweets)
-        }
-    });
+    if (tweets.length > 10) {
+        const ultimosTweets = tweets.slice(-10)
+        response.send(ultimosTweets)
+    }
+    else {
+        response.send(tweets)
+    }
+});
 
 // Criar usuário
 app.post("/sign-up", (request, response) => {
     const novoUsuário =
-        [
-            {
-                username: request.body.username,
-                avatar: request.body.avatar
-             }
-        ]
-    usuarios.push(novoUsuário) 
+    {
+        username: request.body.username,
+        avatar: request.body.avatar
+    }
+
+    usuarios.push(novoUsuário)
     response.send("OK")
 })
 
-app.post("/tweets", (request, response) => {
-    const tweet =
-        [
-            {
-                username: request.body.username,
-                tweet: request.body.tweet
-            }
-        ]
 
-    tweets.push(tweet)
+//Criando tweets
+app.post("/tweets", (request, response) => {
+
+    //Checando se tem conta
+    const TemConta = usuarios.find(u => u.username === request.body.username)
+    const foto = usuarios.find(user => user.username === request.body.username)
+
+    if (!TemConta) {
+        response.send("UNAUTHORIZED")
+    }
+
+    const posttweet =
+    {
+        username: request.body.username,
+        avatar: foto.avatar,
+        tweet: request.body.tweet
+    }
+
+    tweets.push(posttweet)
     response.send("OK")
 })
 
